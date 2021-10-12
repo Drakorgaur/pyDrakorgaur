f9973d11d639925193642daa030c1fe37d2254b5
Mark Ermine <re.markofdark@gmail.com>
2021-10-12 17:00:52 +0200

    added db_set

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -44,7 +44,7 @@ def getTable(message):
     chat_id = message.chat.id
     conn = psycopg2.connect(dbname='testtable', user='remar', password='REmark0712', host='localhost', port='5432')
     cur = conn.cursor()
-    if checkIfTablesExists(conn, cur):
+            if checkIfTablesExists(conn, cur):
         bot.send_message(message.chat.id, "Tables exist")
     else:
         bot.send_message(message.chat.id, "Tables don't exist")
@@ -175,8 +175,8 @@ def createTable(message):
          CREATE TABLE user_lessons (
             user_id INTEGER,
             lessons_id INTEGER,
-            PRIMARY KEY (user_id, lessons_id)
-            CONSTRAINT fk_users FOREIGN KEY(user_id) REFERENCES user(id)
+            PRIMARY KEY (user_id, lessons_id),
+            CONSTRAINT fk_users FOREIGN KEY(user_id) REFERENCES user(id),
             CONSTRAINT fk_lessons FOREIGN KEY(lessons_id) REFERENCES lessons(id)
         );
         """)
