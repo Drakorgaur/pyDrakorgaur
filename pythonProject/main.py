de1ff2de5faeafe3bffc3dcfa6ff407e35aca732
Mark Ermine <re.markofdark@gmail.com>
2021-10-14 12:10:59 +0200

    added db_set

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -402,6 +402,7 @@ def divide(lessons):
     for week_day in ["Monday", "Tuesday", 'Wednesday', 'Thursday', 'Friday']:
         concrete_day = []
         for lesson in lessons:
+            print(lesson[0])
             if week_day == lesson[0]:
                 concrete_day.append(lesson)
         sorted_day[week_day] = concrete_day
