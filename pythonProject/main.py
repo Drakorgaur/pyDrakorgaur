e36e7bd946cb9484eaaf83c6891a0a7c14c27563
Mark Ermine <re.markofdark@gmail.com>
2021-10-23 15:42:36 +0200

    add

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -45,7 +45,7 @@ def compareUserSchedules(message):
     common_lessons = []
     psql_get_user_lessons = (
         """
-        SELECT lessons FROM users WHERE usename = (%s);
+        SELECT lessons FROM users WHERE username = (%s);
         """
     )
     psql_select_lessons_by_id = (
